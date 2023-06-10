3dc8f0fa78467d230b277710772c4419e51be5af
ElianCastro <eliancastro98@gmail.com>
2023-06-09 23:06:55 -0300

    Segunda Practica integradora

--- src/middleware/autenticacion.middleware.js
+++ src/middleware/autenticacion.middleware.js
@@ -1,6 +1,6 @@
 function auth(req, res, next) {
     console.log('auth',req.session)
-    if(req.session?.user?.first_name !== 'fede' || !req.session?.user?.admin === 'admin'){
+    if(req.session?.user?.first_name !== 'elian' || !req.session?.user?.admin === 'admin'){
         return res.status(401).send('Error de autenticación')
     }
     next()
